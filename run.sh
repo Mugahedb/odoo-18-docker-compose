9b05704b0b671ce8508fbf24ed29f55de73e1de3
Mugahedb <mugahed1@hotmail.com>
2026-03-30 00:56:01 +0300

    Update GitHub repository URL in run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ CHAT=$3
 ENVIRONMENT=$4  # New parameter to specify prod or test
 
 # Clone Odoo directory
-git clone --depth=1 https://github.com/minhng92/odoo-18-docker-compose $DESTINATION
+git clone --depth=1 https://github.com/Mugahedb/odoo-18-docker-compose $DESTINATION
 rm -rf $DESTINATION/.git
 
 # Create PostgreSQL directory
